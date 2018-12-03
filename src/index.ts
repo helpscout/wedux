48993f407080b05c2d04fbfd94b334563ac3565a
Jon Q <hello@jonquach.com>
2018-12-02 22:31:31 -0500

    Add connectWithStore to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import createStore from './createStore'
 
 export {default as connect} from './connect'
+export {default as connectWithStore} from './connectWithStore'
 export {default as Provider} from './Provider'
 export {default as combineReducers} from './combineReducers'
 export {default as applyMiddleware} from './applyMiddleware'
